7a4d6e076aa448a381bb697ffa55514893b82469
Eric Hambro <eric.hambro@gmail.com>
2017-05-05 21:40:56 -0400

    liftoff

--- main.m.cpp
+++ main.m.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <eigen3/Eigen/Dense>
 
+#include "load_csv.h"
 
 int main(int argc, char ** argv)
 {
@@ -10,5 +11,8 @@ int main(int argc, char ** argv)
         std::cout << "Arg " << i << " : " << argv[i] << std::endl;
     }
 
+    Eigen::MatrixXd Input = load_csv<Eigen::MatrixXd>("./input/test1.csv");
+
+    std::cout << Input << std::endl;
     return 0;
 }
